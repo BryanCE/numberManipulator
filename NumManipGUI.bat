3be9f3e83a2b341db4e728adc09aa0b3c0f017f4
Bryan <bryanedman86@gmail.com>
2024-10-03 04:00:29 -0600

    updated? I think

--- NumManipGUI.bat
+++ NumManipGUI.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
-"C:\Python312\python.exe" "C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\NumberManipulatorGUI.py"
-exit
+"C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\env\Scripts\python.exe" "C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\NumberManipulatorGUI.py"
 
 @REM C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\build\exe.win-amd64-3.12\NumberManipulatorGUI.exe
